8237e8f29ad2f16978fed402fe3e7ce8385f8eaa
Valentin Barner <ucgvw@student.kit.edu>
2021-11-05 20:08:24 +0100

    run_program fertig

--- ÜB1/p1/print.h
+++
@@ -1,8 +0,0 @@
-#ifndef PRINT_H
-#define PRINT_H
-#include <inttypes.h>
-
-void print_line(int64_t number, char *string);
-
-#endif
-
--- ÜB2/asst02-templates/p1/run_program.c
+++ ÜB2/asst02-templates/p1/run_program.c
@@ -3,39 +3,8 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
-#define ERROR_CODE 127
+#define ERROR_CODE 1
 
-/*remove
-int main(){
-    char *argv1[] = {
-        "/",
-        NULL
-    };
-    int result1 = run_program("/bin/ls", argv1);
-
-    char * argv2[] = {
-        NULL
-    };
-    int result2 = run_program("/bin/false", argv2);
-
-    (void)
-}
-*/
-
-/*
- *Returns size of arrays that are terminated with NULL
- */
-int getArraySize(char *array[]){
-    int size = 0;
-    while (*array != NULL)
-    {
-        size++;
-        array++;
-    }
-
-    return size + 1;
-    
-}
 
 int run_program(char *file_path, char *arguments[])
 {
@@ -45,33 +14,21 @@ int run_program(char *file_path, char *arguments[])
 
     if(pid == 0){
         execvp(file_path, arguments);
-        exit(-1);
+        exit(7);
     } else {
 
         int exitStatus;
-        if(waitpid(pid, &exitStatus, 0) == -1){
-            return ERROR_CODE;
-        } else {
+        int wait_pid = waitpid(pid, &exitStatus, 0);
+        printf("Exitstatus = %d; Wait_Pid %d\n", exitStatus, wait_pid);
+        if(exitStatus == 0){
+            printf("success\n");
             return 0;
+        } else {
+            printf("failure\n");
+            return ERROR_CODE;
         }
     }
 }
-/*
-void schmutz() {
-    int argLength = getArraySize(arguments);
-    
-    //Argument array starting with the file path
-    char *argsWithFilePath[argLength + 1];
-    *argsWithFilePath[0] = *file_path;
 
-    for (size_t i = 0; i < argLength; i++)
-    {
-        *argsWithFilePath[i + 1] = *arguments[i];
-        printf("%s", *argsWithFilePath[i + 1]);
-    }
-    
-    
-}
-*/
 
 
